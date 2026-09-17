6a61562d39b639bc33638ca1538c7aa0b58fc4d0
unknown <izazsqputra@gmail.com>
2026-09-17 19:00:55 +0700

    feat: Create Comment Logic

---
+++ app/Http/Controllers/Comments.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class Comments extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'nama' => ['required', 'string', 'max:100'],
+            'phone' => ['required', 'string', 'max:50'],
+            'comment' => ['required', 'string'],
+        ]);
+
+        Comment::create($data);
+
+        return redirect()->route('index')->with('success', 'komentar telah terkirim');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
